4ac97e08b246d80bfe1e0872bf2ec615b2afc801
Drew <dch528@gmail.com>
2024-03-12 18:19:36 -0400

    Drew H Sequences Lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        fib_sequence = [0, 1]
+        for i in range(2, length):
+            next_element = fib_sequence[i-1] + fib_sequence[i-2]
+            fib_sequence.append(next_element)
+        print(fib_sequence)
+
+
+print_fibonacci(9)
